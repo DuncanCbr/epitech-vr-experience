afe9764f0e6317ae44b1345ebdbf8307b6fe59fe
Duncan Carbonnier <carbonnierduncan@gmail.com>
2026-06-27 03:24:26 +0200

    Feature - mise en place lobby scene
    - mise en place ecran d'affichage des touches au démarrage
    - mise en place d'un affichage smooth

---
+++ Assets/Script/UI/LobbyScene/IntroCanvas.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+public class IntroCanvas : MonoBehaviour
+{
+
+    [SerializeField] private float displayDuration;
+    [SerializeField] private float fadeDuration;
+    [SerializeField] private CanvasGroup canvasGroupImage;
+    [SerializeField] private CanvasGroup canvasGroupBlackScreen;
+    [SerializeField] private GameObject canvasBlackScreen;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(PlayTutorialScreen());
+    }
+
+    private IEnumerator PlayTutorialScreen()
+    {
+        yield return StartCoroutine(Fade(0f, 1f, canvasGroupImage));
+        yield return new WaitForSeconds(displayDuration);
+        yield return StartCoroutine(Fade(1f, 0f, canvasGroupImage));
+        yield return StartCoroutine(Fade(1f, 0f, canvasGroupBlackScreen));
+        gameObject.SetActive(false);
+        canvasBlackScreen.SetActive(false);
+    }
+
+    private IEnumerator Fade(float startAlpha, float endAlpha, CanvasGroup canvasGroup)
+    {
+        float time = 0f;
+
+        while (time < fadeDuration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, endAlpha, time / fadeDuration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        canvasGroup.alpha = endAlpha;
+    }
+
+}
